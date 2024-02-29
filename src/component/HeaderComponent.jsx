d9f52bf12f6ac09f119c133437e5154092051b97
Sugambaj7 <sugambaj123@gmail.com>
2024-02-29 16:19:16 +0545

    added logout functionality

--- src/component/HeaderComponent.jsx
+++ src/component/HeaderComponent.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:4001/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4001/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUsername(null);
+  }
+  console.log(username, "from user anaksdflaksjdlfka");
   return (
     <header>
       <Link to="/">
@@ -10,8 +29,18 @@ export const Header = () => {
       </Link>
 
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new blog</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
